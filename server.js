2b34f01c4fe40a495e60ef70be9d4310ea90cb65
atkaridarshan04 <atkaridarshan04@gmail.com>
2025-05-04 13:29:54 +0530

    resolve 502 render error

--- server.js
+++ server.js
@@ -77,6 +77,6 @@ app.post('/api/notify', cors(), async (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
 });
